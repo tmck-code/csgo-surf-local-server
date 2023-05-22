59aab0fae88c43d45be583d66a3416a436783718
Tom McKeesick <tmck01@gmail.com>
2023-05-22 23:31:30 +1000

    add docker-compose file
    contains the csgo surf server and the mysql database needed for
    SurfTimer

--- run.sh
+++ run.sh
@@ -9,7 +9,7 @@ docker run \
   -e SRCDS_TOKEN="$CSGO_GSLT" \
   -e SRCDS_NET_PUBLIC_ADDRESS=10.0.0.39 \
   -e SRCDS_IP=10.0.0.39 \
-  -e SRCDS_STARTMAP="surf_utopia_njv" \
+  -e SRCDS_STARTMAP="surf_anzchamps" \
   -e SRCDS_TICKRATE=64 \
   -e SRCDS_FPSMAX=120 \
   -e SRCDS_HOSTNAME="arrakis" \
