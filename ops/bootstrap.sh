5a826443a54255b61c59b7c8246347e93f8c389f
Tom McKeesick <tmck01@gmail.com>
2023-05-31 00:59:23 +1000

    get bootstrapper working

--- ops/bootstrap.sh
+++ ops/bootstrap.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-set -euo pipefail
+set -euxo pipefail
 
-CSGO_DIR="$PWD/data/csgo"
+DEST_DIR="${1:-$PWD}"
+CSGO_DIR="$DEST_DIR/csgo"
 SOURCEMOD_DIR="$CSGO_DIR/addons/sourcemod"
 PLUGINS_DIR="$SOURCEMOD_DIR/plugins"
 GAMEDATA_DIR="$SOURCEMOD_DIR/gamedata"
+
+mkdir -p "$CSGO_DIR"
 
 # plugins ---------------------------------------
 # most plugin repos contain a dir or zip that is intended to be copied straight
@@ -16,22 +19,27 @@ GAMEDATA_DIR="$SOURCEMOD_DIR/gamedata"
 
 # - Momentum Mod Ramp Glitch Fix
 echo "- Momentum Mod Ramp Glitch Fix (MomSurfFix)"
-git clone gitgithub.com:GAMMACASE/MomSurfFix.git
-cp -Rv MomSurfFix/addons "$CSGO_DIRPATH"
+git clone git@github.com:GAMMACASE/MomSurfFix.git
+cp -Rv MomSurfFix/addons "$CSGO_DIR"
 rm -rf MomSurfFix
 
+# TODO: this is provided by SurfTimer now?
 # - CSGO Movement Unlocker
 # I've included the files in this repo, they are originally sourced from the
 # AlliedModders forum post here:
 # https://forums.alliedmods.net/showthread.php?t=255298
-echo "- CSGO Movement Unlocker"
-plugin_dir="$PWD/config/plugins/csgo_movement_unlocker"
-cp -v "$plugin_dir/csgo_movement_unlocker.games.txt" "$GAMEDATA_DIR"
-cp -v "$plugin_dir/csgo_movement_unlocker.smx" "$PLUGINS_DIR"
+# echo "- CSGO Movement Unlocker"
+mkdir -p "$GAMEDATA_DIR/" "$PLUGINS_DIR/"
+# plugin_dir="$PWD/ops/csgo_movement_unlocker"
+# cp -v "$plugin_dir/csgo_movement_unlocker.games.txt" "$GAMEDATA_DIR/"
+# cp -v "$plugin_dir/csgo_movement_unlocker.smx" "$PLUGINS_DIR/"
 
+# TODO: this is provided by SurfTimer now?
 # map chooser
-echo "- SurfTimer Map Chooser"
-wget https://github.com/surftimer/SurfTimer-Mapchooser/releases/download/2.0.2/SurfTimer-MC-v2.0.2.zip
+# echo "- SurfTimer Map Chooser"
+# wget https://github.com/surftimer/SurfTimer-Mapchooser/releases/download/2.0.2/SurfTimer-MC-v2.0.2.zip
+# unzip SurfTimer-MC-v2.0.2.zip -d "$SOURCEMOD_DIR/"
+# rm SurfTimer-MC-v2.0.2.zip
 
 # The SurfTimer plugin
 # HUD speedometer
@@ -41,6 +49,8 @@ wget https://github.com/surftimer/SurfTimer-Mapchooser/releases/download/2.0.2/S
 # and more!
 echo "- SurfTimer"
 wget https://github.com/surftimer/SurfTimer/releases/download/1.1.4/SurfTimer.1.1.4.914.6a563cd.SM1.11.zip
+unzip SurfTimer.1.1.4.914.6a563cd.SM1.11.zip -d "$CSGO_DIR/"
+rm SurfTimer.1.1.4.914.6a563cd.SM1.11.zip
 
 # SurfTimer zone coordinates for most surf maps
 echo "- SurfTimer zone coordinates"
@@ -50,10 +60,11 @@ git clone https://github.com/Sayt123/SurfZones
 
 # - install server cfg file
 echo "- surf_server.cfg config file"
-cp -v config/surf_server.cfg "$CSGO_DIR/cfg/"
+mkdir -p "$CSGO_DIR/cfg/"
+cp -v ops/surf_server.cfg "$CSGO_DIR/cfg/"
 
 # - install the surftimer cfg file
 echo "- surftimer.cfg config file"
 mkdir -p "$CSGO_DIR/cfg/sourcemod/surftimer/"
-cp -v config/surftimer.cfg "$CSGO_DIR/cfg/sourcemod/surftimer/"
+cp -v ops/surftimer.cfg "$CSGO_DIR/cfg/sourcemod/surftimer/"
 
