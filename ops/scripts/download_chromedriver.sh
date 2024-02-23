7ec6fc9532c5602ce29008cbbf0f54d765490536
Tom McKeesick <tmck01@gmail.com>
2024-02-23 22:35:41 +1100

    update chromedriver downloader

--- ops/scripts/download_chromedriver.sh
+++ ops/scripts/download_chromedriver.sh
@@ -2,6 +2,8 @@
 
 set -euxo pipefail
 
+PLATFORM=linux64
+
 if ! test -e /usr/bin/google-chrome; then
   # credit: https://www.gregbrisebois.com/posts/chromedriver-in-wsl2/
   sudo apt install -y curl unzip xvfb libxi6 libgconf-2-4
@@ -10,10 +12,22 @@ if ! test -e /usr/bin/google-chrome; then
   google-chrome --version
 fi
 
-CHROME_MAJOR_VERSION=$(google-chrome --version | sed -E "s/.* ([0-9]+)(\.[0-9]+){3}.*/\1/")
-CHROME_DRIVER_VERSION=$(wget -qO- https://googlechromelabs.github.io/chrome-for-testing/LATEST_RELEASE_${CHROME_MAJOR_VERSION} | sed 's/\r$//')
-CHROME_DRIVER_URL=https://edgedl.me.gvt1.com/edgedl/chrome/chrome-for-testing/$CHROME_DRIVER_VERSION/linux64/chromedriver-linux64.zip
-echo $CHROME_DRIVER_URL
-wget $CHROME_DRIVER_URL
+# this is a handy API that's directly recommended by the official chromedriver
+# download page (https://chromedriver.chromium.org/downloads)
+chrome_versions=$(curl -s https://googlechromelabs.github.io/chrome-for-testing/known-good-versions-with-downloads.json)
+# e.g. https://storage.googleapis.com/chrome-for-testing-public/122.0.6261.69/linux64/chromedriver-linux64.zip
+url_for_version=$(
+  echo $chrome_versions \
+    | jq -r ".versions[]
+      | select( .version == \"$(/usr/bin/google-chrome --version | cut -d' ' -f3)\" )
+      | .downloads.chromedriver[]
+      | select( .platform == \"$PLATFORM\" )
+      | .url"
+)
 
-unzip chromedriver-linux64.zip
+if [ -n "${url_for_version:-}" ]; then
+  rm -rf "chromedriver-${PLATFORM}*"
+  wget $url_for_version
+  unzip chromedriver-linux64.zip
+fi
+
